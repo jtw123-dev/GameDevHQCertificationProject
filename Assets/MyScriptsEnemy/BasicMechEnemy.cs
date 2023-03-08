be9eedc6d0abcef892b5f18c14317e661e9e8737
Joshua Watts <joshwatts592@gmail.com>
2023-03-08 15:59:56 -0330

    Added towers

--- Assets/MyScriptsEnemy/BasicMechEnemy.cs
+++ Assets/MyScriptsEnemy/BasicMechEnemy.cs
@@ -8,7 +8,9 @@ public class BasicMechEnemy : Enemy
 
 {
     [SerializeField] private Renderer _renderer;
+    [SerializeField] private Transform _target;
     private float _dissolving;
+    [SerializeField] private bool _slowDown;
     
     // Start is called before the first frame update
     void Start()
@@ -23,14 +25,40 @@ public class BasicMechEnemy : Enemy
     // Update is called once per frame
     void Update()
     {
+     
+        //create a list of towers and choose which one to attack by distance via foreach loop
+
+
+
+        Vector3 directionToFace = _target.position - transform.position;
+        Debug.DrawRay(transform.position, directionToFace, Color.red);
+       // Ray rayOrigin = new Ray (transform.position,transform.forward);
+       // RaycastHit hit;
+        //Debug.DrawRay(rayOrigin.origin, directionToFace,Color.black);
+       // if (Physics.Raycast(rayOrigin,out hit,1))
+       // {
+       //     Debug.Log("hit someting");
+       // }
+
+        transform.rotation = Quaternion.LookRotation(directionToFace);
+
+
+        if (_slowDown ==true)
+        {
+            Time.timeScale = 0.5f;
+        }
+
         if (_health<=0)
         {
             _isDead = true;
+            _anim.applyRootMotion = false;
         }
         if (_isDead==true)
         {
+            _anim.applyRootMotion = false;
             StartCoroutine(DissolveRoutine());
             _agent.isStopped = true;
+            _anim.SetTrigger("Death");
             Destroy(this.gameObject,2.5f);
            
         }
@@ -46,7 +74,16 @@ public class BasicMechEnemy : Enemy
             yield return new WaitForSeconds(0.2f);
 
         }
-        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag=="Tower")
+        {
+            Debug.Log("Attacking");
+        }
+    }
+    public override void  EnemyAttack()
+    {
 
     }
 }
