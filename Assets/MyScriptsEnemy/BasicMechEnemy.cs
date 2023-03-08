b0d662e49e07ae4f707cffc25f370dd3f9a06a2c
Joshua Watts <joshwatts592@gmail.com>
2023-03-08 12:01:09 -0330

    Finished dissolve

--- Assets/MyScriptsEnemy/BasicMechEnemy.cs
+++ Assets/MyScriptsEnemy/BasicMechEnemy.cs
@@ -8,38 +8,45 @@ public class BasicMechEnemy : Enemy
 
 {
     [SerializeField] private Renderer _renderer;
+    private float _dissolving;
     
     // Start is called before the first frame update
     void Start()
     {
         _agent.destination = _waypoints[1].transform.position;
-        // _renderer = GetComponent<Renderer>();
+        _renderer.sharedMaterial.SetFloat("_Clipping_value", 0);
+ 
 
-        _renderer.sharedMaterial.EnableKeyword("Clipping_value");
+       // _renderer.sharedMaterial.EnableKeyword("Clipping_value");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_health<=0)
+        {
+            _isDead = true;
+        }
         if (_isDead==true)
         {
-            float totalTime = 0.2f;
-            Debug.Log("testing");
-            float dissolving =  totalTime++;
-            // _renderer.sharedMaterial.enabledKeywords;
-
-            // _renderer.sharedMaterial.shader = Shader.Find("Shader Graphs/Dissolve");
-
-
-            _renderer.sharedMaterial.SetFloat("_Clipping_value", dissolving);
-            Destroy(this.gameObject,2);
+            StartCoroutine(DissolveRoutine());
+            _agent.isStopped = true;
+            Destroy(this.gameObject,2.5f);
            
         }
     }
-
-    void CheckShaderKeyWordState()
+    private IEnumerator DissolveRoutine()
     {
-        //var shader = material
+        while(true)
+        {
+            _dissolving += 0.1f *Time.deltaTime;
+            
+            
+            _renderer.sharedMaterial.SetFloat("_Clipping_value", _dissolving);
+            yield return new WaitForSeconds(0.2f);
+
+        }
+        
+
     }
 }
